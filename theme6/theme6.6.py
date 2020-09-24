5017f1a915e2d45d1f9825caa4d9b174d572ac90
Эвелина <71436373+evelinasvarovskaya@users.noreply.github.com>
2020-09-25 02:17:03 +0300

    Add files via upload

---
+++ theme6/theme6.6.py
@@ -0,0 +1,6 @@
+print("Введите число:")
+a = int(input())
+b = a*a
+b = b*b
+b = b*b
+print("Новое число:", b)
---
+++ theme6/theme6.7.py
@@ -0,0 +1,8 @@
+print("Введите число:")
+a = int(input())
+c = a*a
+b = c*a
+b = c*b #5
+c = b*b #10
+c = c*b
+print("Результат:", c)
